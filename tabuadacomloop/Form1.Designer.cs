c30e384e23e886731c62009a7c549ab10679662a
leonardo120706 <leonardo.goliveira15@senacsp.edu.br>
2025-03-24 17:15:30 -0300

    Add files via upload

---
+++ tabuadacomloop/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace tabuadacomloop
+{
+    partial class frmTabudaLoop
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelFundo = new System.Windows.Forms.Panel();
+            this.panelEsquerda = new System.Windows.Forms.Panel();
+            this.panelDireita = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lstTabuada = new System.Windows.Forms.ListBox();
+            this.panelFundo.SuspendLayout();
+            this.panelEsquerda.SuspendLayout();
+            this.panelDireita.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelFundo
+            // 
+            this.panelFundo.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panelFundo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelFundo.Controls.Add(this.panelDireita);
+            this.panelFundo.Controls.Add(this.panelEsquerda);
+            this.panelFundo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelFundo.Location = new System.Drawing.Point(0, 0);
+            this.panelFundo.Name = "panelFundo";
+            this.panelFundo.Size = new System.Drawing.Size(800, 450);
+            this.panelFundo.TabIndex = 0;
+            // 
+            // panelEsquerda
+            // 
+            this.panelEsquerda.BackColor = System.Drawing.Color.PeachPuff;
+            this.panelEsquerda.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelEsquerda.Controls.Add(this.lstTabuada);
+            this.panelEsquerda.Location = new System.Drawing.Point(10, 24);
+            this.panelEsquerda.Name = "panelEsquerda";
+            this.panelEsquerda.Size = new System.Drawing.Size(366, 412);
+            this.panelEsquerda.TabIndex = 0;
+            // 
+            // panelDireita
+            // 
+            this.panelDireita.BackColor = System.Drawing.Color.PeachPuff;
+            this.panelDireita.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelDireita.Controls.Add(this.btnExecutar);
+            this.panelDireita.Controls.Add(this.txtNumero);
+            this.panelDireita.Controls.Add(this.lblTitulo);
+            this.panelDireita.Location = new System.Drawing.Point(413, 24);
+            this.panelDireita.Name = "panelDireita";
+            this.panelDireita.Size = new System.Drawing.Size(373, 412);
+            this.panelDireita.TabIndex = 1;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(61, 21);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(243, 37);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Insira um número";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(100, 80);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(157, 62);
+            this.txtNumero.TabIndex = 1;
+            this.txtNumero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnExecutar.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.ForeColor = System.Drawing.Color.White;
+            this.btnExecutar.Location = new System.Drawing.Point(88, 185);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(189, 85);
+            this.btnExecutar.TabIndex = 2;
+            this.btnExecutar.Text = "Executar Tabuada";
+            this.btnExecutar.UseVisualStyleBackColor = false;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // lstTabuada
+            // 
+            this.lstTabuada.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstTabuada.FormattingEnabled = true;
+            this.lstTabuada.ItemHeight = 28;
+            this.lstTabuada.Location = new System.Drawing.Point(25, 21);
+            this.lstTabuada.Name = "lstTabuada";
+            this.lstTabuada.Size = new System.Drawing.Size(311, 340);
+            this.lstTabuada.TabIndex = 0;
+            // 
+            // frmTabudaLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelFundo);
+            this.Name = "frmTabudaLoop";
+            this.Text = "Tabuda Loop";
+            this.panelFundo.ResumeLayout(false);
+            this.panelEsquerda.ResumeLayout(false);
+            this.panelDireita.ResumeLayout(false);
+            this.panelDireita.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelFundo;
+        private System.Windows.Forms.Panel panelDireita;
+        private System.Windows.Forms.Panel panelEsquerda;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ListBox lstTabuada;
+    }
+}
+
---
+++ tabuadacomloop/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabuadacomloop
+{
+    public partial class frmTabudaLoop : Form
+    {
+        public frmTabudaLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            //Limpar a lista antes de adicionar novos resultados
+            lstTabuada.Items.Clear();
+
+            int numeroInserido = Convert.ToInt32(txtNumero.Text);
+            //Adicionar a tabuada para número inserido
+            for (int i = 1; i < 11; i++)
+            {
+                lstTabuada.Items.Add(numeroInserido + "x" + i + "=" + (numeroInserido * i));
+            }
+            
+        }
+    }
+}
