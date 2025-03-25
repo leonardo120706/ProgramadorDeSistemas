e208052d4a73936fad950d8833d3fd6172515130
leonardo120706 <leonardo.goliveira15@senacsp.edu.br>
2025-03-25 14:49:55 -0300

    Add files via upload

---
+++ arrayunidimencional-vetor/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrayunidimencional_vetor
+{
+    public partial class FrmVetor : Form
+    {
+        public FrmVetor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestar_Click(object sender, EventArgs e)
+        {
+            //matriz de uma dimensão, tambem chamanda de vetor
+            string[] pecasComputador = { "Mouse", "Teclado", "Monitor", "Gabinete", "Camera" };
+
+            //MessageBox.Show(pecasComputador[2]);
+
+            foreach (string peca in pecasComputador)
+                MessageBox.Show(peca);
+        }
+    }
+}
