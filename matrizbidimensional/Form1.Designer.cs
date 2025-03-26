5733921f7d9d90f9dd08e3cb454c7f90bd3c9fb2
leonardo120706 <leonardo.goliveira15@senacsp.edu.br>
2025-03-26 16:20:12 -0300

    "matriz"

---
+++ matrizbidimensional/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace marizbidimensionl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnMaiorValor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(113, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mostrar matriz";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(44, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Entre com os valores da matriz";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 176);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 88);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btnMaiorValor
+            // 
+            this.btnMaiorValor.BackColor = System.Drawing.Color.DarkRed;
+            this.btnMaiorValor.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaiorValor.ForeColor = System.Drawing.Color.White;
+            this.btnMaiorValor.Location = new System.Drawing.Point(81, 298);
+            this.btnMaiorValor.Name = "btnMaiorValor";
+            this.btnMaiorValor.Size = new System.Drawing.Size(127, 23);
+            this.btnMaiorValor.TabIndex = 4;
+            this.btnMaiorValor.Text = "Mostrar valor maior";
+            this.btnMaiorValor.UseVisualStyleBackColor = false;
+            this.btnMaiorValor.Click += new System.EventHandler(this.btnMaiorValor_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(354, 348);
+            this.Controls.Add(this.btnMaiorValor);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnMaiorValor;
+    }
+}
+
---
+++ matrizbidimensional/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace marizbidimensionl
+{
+    public partial class Form1 : Form
+    {
+        double[,] matriz = new double[3, 3];
+        int i = 0;
+        int j = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                matriz[i, j] = Double.Parse(textBox1.Text);
+                j++;
+                if (j == 3)
+                {
+                    i++;
+                    j = 0;
+                }
+                textBox1.Text = "";
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i <= 2; i++) //percorre a linha
+            {
+                for (j = 0; j <= 2; j++)
+                {
+                    textBox2.Text += matriz[i, j].ToString() + " ";
+                }
+                textBox2.Text += Environment.NewLine;
+            }
+        }
+
+        private void btnMaiorValor_Click_1(object sender, EventArgs e)
+        {
+
+            double maiorValor = int.MinValue;
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    if (matriz[i, j] > maiorValor)
+                    {
+                        maiorValor = matriz[i, j];
+                    }
+                }
+            }
+            MessageBox.Show("O maior valor na matriz é: " + maiorValor, "Maior Valor");
+        }
+    }
+}
